13a18875c02db0a47530f3a160b68bb2878092b6
Inta <782720519@qq.com>
2019-03-11 11:01:39 +0800

    服务器测试

---
+++ untitled/src/tencent/Test.java
@@ -0,0 +1,12 @@
+package tencent;
+
+/**
+ * @author inta
+ * @date 2019/3/11
+ * @describe
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello,测试Github上传更新");
+    }
+}
