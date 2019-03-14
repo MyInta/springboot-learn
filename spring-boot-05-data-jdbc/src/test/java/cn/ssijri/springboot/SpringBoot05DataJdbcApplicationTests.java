73e175b9a19cfe59b0da3c6903f477d4fa989675
Inta <782720519@qq.com>
2019-03-14 16:43:27 +0800

    3-14

---
+++ spring-boot-05-data-jdbc/src/test/java/cn/ssijri/springboot/SpringBoot05DataJdbcApplicationTests.java
@@ -0,0 +1,31 @@
+package cn.ssijri.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot05DataJdbcApplicationTests {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    public void contextLoads() throws SQLException {
+
+        //class com.zaxxer.hikari.HikariDataSource
+        System.out.println(dataSource.getClass());
+        Connection connection = dataSource.getConnection();
+        //HikariProxyConnection@322100932 wrapping com.mysql.cj.jdbc.ConnectionImpl@544e3679
+        System.out.println(connection);
+        connection.close();
+    }
+
+}
